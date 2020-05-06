0b6279f6d7e84d22d850396043c33f718e75b7c8
Tobias Rittweiler <tobias.rittweiler@contractors.roche.com>
2020-05-06 14:20:13 +0200

    bash/profile: set XDG_DATA_DIRS for .desktop files of locally installed software.

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+if ! [ "$UID" -eq 0 ]; then
+    exec sudo $0 "$@"
+fi
 
-# Cousine font, used by Emacs config.
-sudo apt install fonts-croscore
+declare -a packages=(
+    fonts-croscore              # Cousine font
+    screen
+)
+
+set -x
+apt update
+apt install -y ${packages[@]}
