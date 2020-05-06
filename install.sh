bd2260906b309a815294f36862233c412eae428d
Tobias Rittweiler <tobias.rittweiler@contractors.roche.com>
2020-05-06 14:20:39 +0200

    ubuntu/install-packages.sh -> install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
+set -e
+
+source /etc/lsb-release
+if ! [ "${DISTRIB_ID}" == "Ubuntu" ]; then
+    echo "Error: script only works with Ubuntu" >&2
+    exit 1
+fi
+
 if ! [ "$UID" -eq 0 ]; then
     exec sudo $0 "$@"
 fi
 
 declare -a packages=(
+    autoconf
+    build-essential
+    bear			# LD_PRELOAD tool to generate compile_commands.json
     fonts-croscore              # Cousine font
+    clang-9
+    clang-format-9
+    clang-tidy-9
+    clang-tools-9
+    clangd-9
+    htop
+    libgif-dev			# for compiling Emacs
+    libgtk-3-dev		# ditto
+    libjpeg-dev			# ditto
+    libssl-dev			# ditto
+    libxpm-dev			# ditto
     screen
+    texinfo			# makeinfo
 )
 
 set -x
